011d96c273ed79b0c1608a5405c65b129b429b09
Federico Marzocchi <hello@mrz.io>
2017-08-31 14:31:13 +0200

    playing with ffmpeg video recording

---
+++ NOTE.md
@@ -0,0 +1,4 @@
+Record videos with
+
+    ffmpeg -f x11grab -video_size 1280x720 -i :99 -codec:v libx264 -r 12 /movies/out.mp4
+
--- chrome-xvfb/cmd.sh
+++ chrome-xvfb/cmd.sh
@@ -1,6 +1,6 @@
 export DISPLAY=:99.0
 
-Xvfb $DISPLAY -screen 0 1280x720x16 &> /var/log/xvfb.log &
+Xvfb ${DISPLAY} -screen 0 ${SCREEN_SIZE:-1280x720}x24 &> /var/log/xvfb.log &
 
 java -Dwebdriver.chrome.logfile=/var/log/chrome.log \
     -jar /opt/selenium-server-standalone.jar
